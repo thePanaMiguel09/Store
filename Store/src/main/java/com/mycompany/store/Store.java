d5a410bb7147cea9249d99b8a57ae273a0857f06
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2024-10-16 08:10:12 -0500

    First commit

---
+++ Store/src/main/java/com/mycompany/store/Store.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.store;
+
+/**
+ *
+ * @author migui
+ */
+public class Store {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
