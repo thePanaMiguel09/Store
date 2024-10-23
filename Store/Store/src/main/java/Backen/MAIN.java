fd2e74bb4b9a3b34b8228e0c570359eba53e2723
L0pezzz <louisfeliphetorreslopez@gmail.com>
2024-10-23 16:08:19 -0500

    sout("hola")

---
+++ Store/Store/src/main/java/Backen/MAIN.java
@@ -0,0 +1,13 @@
+
+package Backen;
+
+public class MAIN {
+
+    public static void main(String[] args) {
+        
+            System.out.println("HOLA");   
+            System.out.println("hi");
+        
+    }
+    
+}
